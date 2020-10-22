5eee935b243a0254d9fdf4f138042c7312d28db6
shradhaN <shradha.neupane@yahoo.com>
2020-10-22 22:14:18 +0545

    v2-api: View all wishlists and their products

--- app/controllers/spree/api/v2/storefront/wishlists_controller.rb
+++ app/controllers/spree/api/v2/storefront/wishlists_controller.rb
@@ -6,10 +6,12 @@ module Spree
           include Spree::Api::V2::CollectionOptionsHelpers
 
           def index
-            render_serialized_payload { serialize_collection(paginated_collection) }
+            @wishlists = current_wishlist_user.wishlists.page(params[:page]).per(params[:per_page])
+            render_serialized_payload { serialize_collection(@wishlists) }
           end
 
           def show
+            authorize! :show, Spree::Wishlist
             render_serialized_payload { serialize_resource(resource) }
           end
 
@@ -34,6 +36,19 @@ module Spree
           def collection
             collection_finder.new(scope: scope, params: params).execute
           end
+
+          # to allow managing of other users' list by admins
+        def current_wishlist_user
+          @current_wishlist_user ||= begin
+            if params[:user_id] && @current_user_roles.include?('admin')
+              Spree.user_class.find(params[:user_id])
+            else
+              # if the API user is not an admin, or didn't ask for another user,
+              # return themselves.
+              spree_current_user
+            end
+          end
+        end
 
           def resource
             # TODO @prakash: we may not need permalink finder
